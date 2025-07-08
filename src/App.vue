84a4da7473ec7b7c1a86cd1f8ef01a4d3fad6cd7
Macknive <kevin.corpuz@virtusize.com>
2025-07-08 19:23:41 +0800

    initialize

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div
+    class="flex justify-between items-center h-16 bg-gray-800 text-white px-4"
+  >
+    <div class="w-14 flex-none">left</div>
+
+    <div class="flex-row">
+      mid
+
+      <div>
+        search via tags
+        <input
+          type="text"
+          class="border border-gray-300 rounded px-2 py-1"
+          placeholder="Search..."
+        />
+      </div>
+      <div class="flex flex-wrap gap-2">
+        <img src="https://picsum.photos/300/200" alt="" />
+        <img src="https://picsum.photos/300/200" alt="" />
+        <img src="https://picsum.photos/300/200" alt="" />
+        <img src="https://picsum.photos/300/200" alt="" />
+        <img src="https://picsum.photos/300/200" alt="" />
+        <img src="https://picsum.photos/300/200" alt="" />
+        <img src="https://picsum.photos/300/200" alt="" />
+        <img src="https://picsum.photos/300/200" alt="" />
+        <img src="https://picsum.photos/300/200" alt="" />
+        <img src="https://picsum.photos/300/200" alt="" />
+        <img src="https://picsum.photos/300/200" alt="" />
+        <img src="https://picsum.photos/300/200" alt="" />
+        <img src="https://picsum.photos/300/200" alt="" />
+        <img src="https://picsum.photos/300/200" alt="" />
+        <img src="https://picsum.photos/300/200" alt="" />
+        <img src="https://picsum.photos/300/200" alt="" />
+      </div>
+    </div>
+
+    <div class="w-14 flex-none">right</div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+div {
+  height: 100%;
+}
+</style>
